c8af9c26e125c99dd21bfa544382115f36dc913d
Piotr Kulbaka <petr.kulbaka@banuba.com>
2023-10-13 09:51:46 +0300

    1.8.0-10

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let bnbPackageVersion: Version = "1.8.0-9"
+let bnbPackageVersion: Version = "1.8.0-10"
 
 let package = Package(
     name: "BNBLips",
@@ -13,10 +13,10 @@ let package = Package(
             name: "BNBLips",
             targets: [
                 "BNBLips",
-                "BNBLipsBNBSdkCore",
-                "BNBLipsBNBEffectPlayer",
-                "BNBLipsBNBScripting",
-                "BNBLipsBNBFaceTracker"
+                "BNBSdkCore",
+                "BNBEffectPlayer",
+                "BNBScripting",
+                "BNBFaceTracker"
             ]
         ),
     ],
@@ -43,42 +43,6 @@ let package = Package(
             name: "BNBLips",
             url: "https://d2cm7wtcqqw29.cloudfront.net/1.8.0-58-g46079e7d74/BNBLips.zip",
             checksum: "aa0988bde661a16a029edcbd8c59c28eace694e766887c23966dc99955675c85"
-        ),
-        .target(
-            name: "BNBLipsBNBSdkCore",
-            dependencies: [
-                .product(
-                    name: "BNBSdkCore",
-                    package: "BNBSdkCore"
-                ),
-            ]
-        ),
-        .target(
-            name: "BNBLipsBNBEffectPlayer",
-            dependencies: [
-                .product(
-                    name: "BNBEffectPlayer",
-                    package: "BNBEffectPlayer"
-                ),
-            ]
-        ),
-        .target(
-            name: "BNBLipsBNBScripting",
-            dependencies: [
-                .product(
-                    name: "BNBScripting",
-                    package: "BNBScripting"
-                ),
-            ]
-        ),
-        .target(
-            name: "BNBLipsBNBFaceTracker",
-            dependencies: [
-                .product(
-                    name: "BNBFaceTracker",
-                    package: "BNBFaceTracker"
-                ),
-            ]
         ),
     ]
 )
